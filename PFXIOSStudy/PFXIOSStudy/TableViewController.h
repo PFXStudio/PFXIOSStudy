c82a204ca1c1a62ffe5348666decd88dee4afb2b
PFXStudio <pfxstudio21@gmail.com>
2016-04-21 21:06:37 +0900

    Add Project

---
+++ PFXIOSStudy/PFXIOSStudy/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  PFXIOSStudy
+//
+//  Created by PFXStudio on 2016. 4. 21..
+//  Copyright © 2016년 PFXStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@end
---
+++ PFXIOSStudy/PFXIOSStudy/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PFXIOSStudy
+//
+//  Created by PFXStudio on 2016. 4. 21..
+//  Copyright © 2016년 PFXStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
